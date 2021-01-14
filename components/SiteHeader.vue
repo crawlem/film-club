678f3e124cfbe030308cbda4b511f42241e55ab8
Mark Crawley <mark@crawleyweb.co.uk>
2021-01-14 17:26:55 +0000

    Playing with CSS

---
+++ components/SiteHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="site-header">
+    <a href="/" class="site-logo-link">
+      <h1 class="site-logo">
+        <img src="/images/logo.svg" width="24" height="24">
+        Film Club
+      </h1>
+    </a>
+    <nav class="main-nav">
+      <ul>
+        <a href="/">
+          Next meeting
+        </a>
+        <a href="/backlog">
+          Backlog
+        </a>
+        <a href="/suggest">
+          Suggest a film
+        </a>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'SiteHeader'
+}
+</script>
+
+<style>
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -14,12 +14,16 @@ export default {
       { name: 'robots', content: 'noindex' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect', href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:wght@200;400;600&display=swap' }
     ]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [],
+  css: [
+    '~/assets/css/main.scss'
+  ],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
@@ -37,7 +41,6 @@ export default {
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
-    'bootstrap-vue/nuxt',
     '@nuxt/http'
   ],
 
