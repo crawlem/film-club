3590d64eaa6f79c8f493f1b32faea3bed6ff8583
Mark Crawley <mark.crawley@specsavers.com>
2021-03-06 15:04:17 +0000

    Added half star ratings

--- components/FilmRating.vue
+++ components/FilmRating.vue
@@ -1,6 +1,7 @@
 <template>
   <span class="rating" :class="'rating-' + stars">
-    <span v-for="i in stars" :key="i">★</span><span v-for="x in (5 - stars)" :key="x">☆</span>
+    <span v-for="i in Math.floor(stars / 2)" :key="i">★</span><span v-if="stars % 2">½</span>
+    <!-- <span v-for="x in (5 - stars)" :key="x">☆</span> -->
   </span>
 </template>
 
