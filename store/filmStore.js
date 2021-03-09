cd1c006e7e5d7e99ebde5a89c34b92c9a36f0cab
Mark Crawley <mark@crawleyweb.co.uk>
2021-03-09 14:57:23 +0000

    Preload TMDB data and use TMDB poster images

--- store/filmStore.js
+++ store/filmStore.js
@@ -17,6 +17,9 @@ export const actions = {
   async loadAllData (context) {
     // Only load if we haven't already got data
     if (context.state.films.length === 0) {
+      // eslint-disable-next-line
+      console.log('Reloading films data')
+
       // Load all films that have a status assigned
       const filmsResponse = await this.$airtable.get('/Films?filterByFormula=NOT({Status} = \'\')') // &maxRecords=6
       const filmsJson = await filmsResponse.json()
@@ -35,11 +38,26 @@ export const actions = {
 
       // Add meeting details to films
       filmsJson.records.forEach(function (film) {
+        // Meeting info
         const meeting = meetingsJson.records.find(meeting => meeting.fields.Film[0] === film.id)
         if (meeting) {
           film.meeting = meeting
         }
       })
+
+      // Add TMDB info to films
+      for (const film of filmsJson.records) {
+        // Load details of the film from TMDB
+        const tmdbId = film.fields['TMDB ID']
+        const tmdbResponse = await this.$tmdb.get('/movie/' + tmdbId + '?api_key=' + this.$config.TMDB_API_KEY)
+        const tmdbJson = await tmdbResponse.json()
+        film.tmdb = tmdbJson
+
+        // Load details of cast/crew from TMDB
+        const tmdbCreditsResponse = await this.$tmdb.get('/movie/' + tmdbId + '/credits?api_key=' + this.$config.TMDB_API_KEY)
+        const tmdbCreditsJson = await tmdbCreditsResponse.json()
+        film.credits = tmdbCreditsJson
+      }
 
       // Save to state
       context.commit('STORE_FILMS', filmsJson.records)
