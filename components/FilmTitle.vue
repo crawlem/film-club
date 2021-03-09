389fe5d12ec9ced655c3a1b5ffbe9770a03de95b
Mark Crawley <mark@crawleyweb.co.uk>
2021-03-09 15:36:59 +0000

    Use TMDB release date for year (#41)

---
+++ components/FilmTitle.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>{{ title }} <span v-if="year">({{ year }})</span></span>
+</template>
+<script>
+import Moment from 'moment'
+export default {
+  name: 'FilmTitle',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    releaseDate: {
+      type: String,
+      default: ''
+    }
+  },
+
+  computed: {
+    year () {
+      const rawDate = this.releaseDate
+      return (Moment(new Date(rawDate)).isValid()) ? Moment(new Date(rawDate)).format('YYYY') : rawDate
+    }
+  }
+}
+</script>
