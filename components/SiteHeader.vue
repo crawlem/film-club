549d37ea4f1abeb4da3f84d1f0d35e7b0d5de43c
Mark Crawley <mark@crawleyweb.co.uk>
2021-03-11 15:59:41 +0000

    Add alt to logo image

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -3,23 +3,10 @@
     <section>
       <NuxtLink to="/" class="site-logo-link">
         <h1 class="site-logo">
-          <img src="/images/logo.svg" width="24" height="24">
+          <img src="/images/logo.svg" width="24" height="24" alt="Logo">
           Osterlies Film Club
         </h1>
       </NuxtLink>
-      <!-- <nav class="main-nav">
-        <ul>
-          <NuxtLink to="/">
-            Next meeting
-          </NuxtLink>
-          <NuxtLink to="/backlog">
-            Backlog
-          </NuxtLink>
-          <NuxtLink to="/suggest">
-            Suggest a film
-          </NuxtLink>
-        </ul>
-      </nav> -->
     </section>
   </header>
 </template>
