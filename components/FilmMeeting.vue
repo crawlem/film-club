c07f5369f71c784dc55739f456f295e96865838a
Mark Crawley <mark@crawleyweb.co.uk>
2021-05-04 12:19:14 +0100

    Fixed sort order on history for Apr meetings

--- components/FilmMeeting.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="h-event">
-    <h1 class="p-name">Meeting date</h1>
-    <p><time class="dt-start" datetime="2020-12-27">27th December 2020</time></p>
-    <p class="p-summary">Description</p>
-</template>
