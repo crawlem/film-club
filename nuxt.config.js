5f7355152b8e2ae736b4dfabdf7e9af1d7b8a3e1
Mark Crawley <mark.crawley@specsavers.com>
2021-05-05 19:20:59 +0100

    Replaced Nuxt/HTTP with Airtable JavaScript client

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,7 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
-    '~/plugins/tmdb',
-    '~/plugins/airtable'
+    '~/plugins/tmdb'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
